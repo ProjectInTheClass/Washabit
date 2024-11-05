1e310ca93095a5e589525d796a5a2a80b6c36b64
Friedonion <rkdwnd333@naver.com>
2024-11-05 11:49:43 +0900

    create project

---
+++ Washabit/Washabit/WashabitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WashabitApp.swift
+//  Washabit
+//
+//  Created by 강현중 on 11/5/24.
+//
+
+import SwiftUI
+
+@main
+struct WashabitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
